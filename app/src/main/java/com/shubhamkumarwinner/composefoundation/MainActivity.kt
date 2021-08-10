30c718d3d8a2d5da5c786020c5e7b09aaadf9596
Shubham <shubhamkumarwinner@gmail.com>
2021-08-10 17:27:20 +0530

    BaseTextField

--- app/src/main/java/com/shubhamkumarwinner/composefoundation/MainActivity.kt
+++ app/src/main/java/com/shubhamkumarwinner/composefoundation/MainActivity.kt
@@ -3,10 +3,13 @@ package com.shubhamkumarwinner.composefoundation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.TextField
+import androidx.compose.runtime.*
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import com.shubhamkumarwinner.composefoundation.ui.theme.ComposeFoundationTheme
 
@@ -17,11 +20,25 @@ class MainActivity : ComponentActivity() {
             ComposeFoundationTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    BaseTextFieldDemo()
                 }
             }
         }
     }
+}
+
+//baseTextField
+@Composable
+fun BaseTextFieldDemo() {
+    var textState by remember { mutableStateOf(TextFieldValue()) }
+
+    Column {
+        TextField(value = textState, onValueChange = {
+            textState = it
+        })
+        Text("The text field has this text: " + textState.text)
+    }
+
 }
 
 @Composable
@@ -33,6 +50,6 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     ComposeFoundationTheme {
-        Greeting("Android")
+        BaseTextFieldDemo()
     }
 }
