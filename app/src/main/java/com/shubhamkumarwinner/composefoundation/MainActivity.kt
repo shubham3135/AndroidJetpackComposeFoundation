4b5ca02d402e3e1612c30396af56925f36d5d35b
Shubham <shubhamkumarwinner@gmail.com>
2021-08-10 17:35:18 +0530

    lazyColumn

--- app/src/main/java/com/shubhamkumarwinner/composefoundation/MainActivity.kt
+++ app/src/main/java/com/shubhamkumarwinner/composefoundation/MainActivity.kt
@@ -1,19 +1,23 @@
 package com.shubhamkumarwinner.composefoundation
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.shubhamkumarwinner.composefoundation.ui.theme.ComposeFoundationTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,10 +27,42 @@ class MainActivity : ComponentActivity() {
             ComposeFoundationTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    ImageResourceDemo()
+                    LazyColumnDemo()
                 }
             }
         }
+    }
+}
+
+//lazyColumn
+@Composable
+fun LazyColumnDemo() {
+    val list = listOf(
+        "A", "B", "C", "D"
+    ) + ((0..100).map { it.toString() })
+    LazyColumn(modifier = Modifier.fillMaxHeight()) {
+        items(items = list, itemContent = { item ->
+            Log.d("COMPOSE", "This get rendered $item")
+            when (item) {
+                "A" -> {
+                    Text(text = item, style = TextStyle(fontSize = 80.sp))
+                }
+                "B" -> {
+                    Button(onClick = {}) {
+                        Text(text = item, style = TextStyle(fontSize = 80.sp))
+                    }
+                }
+                "C" -> {
+                    //Do Nothing
+                }
+                "D" -> {
+                    Text(text = item)
+                }
+                else -> {
+                    Text(text = item, style = TextStyle(fontSize = 80.sp))
+                }
+            }
+        })
     }
 }
 
@@ -60,6 +96,6 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     ComposeFoundationTheme {
-        ImageResourceDemo()
+        LazyColumnDemo()
     }
 }
